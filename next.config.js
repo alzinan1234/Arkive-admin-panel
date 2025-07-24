42611c20ed0cbca01dab0b37316884f9e0e1c1c7
alzinan1234 <pappurohan1100@gmail.com>
2025-07-24 09:08:32 +0600

    update

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       "via.placeholder.com", // Add the domain for external images
       "images.pexels.com", // Add images.pexels.com for external image support
       "placehold.co", // Add placehold.co for external image support
+      "www.pexels.com", // Add www.pexels.com for external image support
     ],
   },
 };
